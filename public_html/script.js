77977303ce45b6e9035783b430e990b7fc44decc
Jalen Thornton <sk8erboy042300@yahoo.com>
2015-01-15 15:03:29 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -5,13 +5,52 @@
  */
 
 
-$("documents").ready(function(){
-    $(".Money").css("background-color","gray");
+$('document').ready(function(){
     
-    $('p:last').css('color', 'red');
+    $('#superHumans').accordion({header: "h3"});
+    
+    $('.what').css('background-color', 'gray');
+    
+    $('div:last p:nth-child(3)').css('background-color', 'black');
+    
+    $('p:last').css('color', 'black');
     
     $('p:gt(1)').css('border-style', 'solid');
     
-    $('div:last p:nth-child(3)').css('background-color','cyan')
+    $('p:first').css({'background-color':'black', 'color':'white'});
     
+    $('h1').bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe);
+    
+    $('h1').bind('click',mouseClick);
+    
+    $('replaceWText').bind('click',replaceWText);
+    
+    $('#randPara').bind('click', addAPara);
+    
+    $('#removePara').bind('click', removeAPara);
 });
+
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+
+function addAPara(){
+    $('#randPara').append('<p> $Get Money$ </p>');
+}
+
+function replaceWText(){
+    $('#replaceWText').text('');
+}
+
+function mouseOverMe(){
+    $('h1').html('Project');
+}
+
+function mouseOutMe(){
+    $('h1').html('About me');
+}
+
+function mouseClick(){
+    
+    $('p').html('DO U LIKE IT?');
+}
